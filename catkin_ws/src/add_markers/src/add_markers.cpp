d50dfd1d3d0e7a42b512a3e4b445c05462de2766
eogiesoba <eogiesoba@gmail.com>
2019-05-19 01:02:32 +0000

    Update add_markers node and build files

--- catkin_ws/src/add_markers/src/add_markers.cpp
+++ catkin_ws/src/add_markers/src/add_markers.cpp
@@ -1,56 +1,66 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
+#include <nav_msgs/Odometry.h>
 
+// Create initial odometry values to be used in add_markers node.
+float x_Odom = 0.0;
+float y_Odom = 0.0;
+// Create callback function for updated odometry values to be outputed.
+void odom_callback(const nav_msgs::Odometry::ConstPtr& msg) {
+  ::x_Odom = msg->pose.pose.position.x;
+  ::y_Odom = msg->pose.pose.position.y;
+}
+//This will run the main virtual object logic to place object in 1st and 2nd location.
 int main( int argc, char** argv )
 {
   ros::init(argc, argv, "add_markers");
   ros::NodeHandle n;
   ros::Rate r(1);
   ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+  ros::Subscriber Odom_sub = n.subscribe("/odom", 1000, odom_callback);
 
   // Set our initial shape type to be a cube
   uint32_t shape = visualization_msgs::Marker::CUBE;
+  // Create initial x,y,delta, and radius values
+  float x_length = 0.0;
+  float y_length = 0.0;
+  float delta = 0.5;
+  float rad = 0.0;
+  // Define position and orientation of 2 robot location goals. 
+  float x_goal = 0.5;
+  float y_goal = 0.0;
+  float w_goal = 1.0;
+  float x_goal2 = 0.0;
+  float y_goal2 = 0.5;
+  float w_goal2 = 1.0;
+
+  // Initialize marker
+  visualization_msgs::Marker marker;
+  // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+  marker.header.frame_id = "map";
+  marker.header.stamp = ros::Time::now();
+  // Set the namespace and id for this marker.  This serves to create a unique ID
+  // Any marker sent with the same namespace and id will overwrite the old one
+  marker.ns = "basic_shapes";
+  marker.id = 0;
+  // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
+  marker.type = shape;
+
+  // Give the marker a color of orange.
+  marker.color.r = 1.0f;
+  marker.color.g = 0.65f;
+  marker.color.b = 0.0f;
+  marker.color.a = 1.0;
+  // Give the marker a 1 cubic meter size. 
+  marker.scale.x = 1.0;
+  marker.scale.y = 1.0;
+  marker.scale.z = 1.0;
+
+  // Create state vairable.
+  uint32_t state_var = 0;
 
   while (ros::ok())
   {
-    visualization_msgs::Marker marker;
-    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-    marker.header.frame_id = "map";
-    marker.header.stamp = ros::Time::now();
-
-    // Set the namespace and id for this marker.  This serves to create a unique ID
-    // Any marker sent with the same namespace and id will overwrite the old one
-    marker.ns = "add_markers";
-    marker.id = 0;
-
-    // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
-    marker.type = shape;
-
-    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-    marker.action = visualization_msgs::Marker::ADD;
-
-    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-    marker.pose.position.x = 0;
-    marker.pose.position.y = 0;
-    marker.pose.position.z = 0;
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
-
-    // Set the scale of the marker -- 1x1x1 here means 1m on a side
-    marker.scale.x = 1.0;
-    marker.scale.y = 1.0;
-    marker.scale.z = 1.0;
-
-    // Set the color -- be sure to set alpha to something non-zero!
-    marker.color.r = 0.0f;
-    marker.color.g = 1.0f;
-    marker.color.b = 0.0f;
-    marker.color.a = 1.0;
-
-    marker.lifetime = ros::Duration();
-
     // Publish the marker
     while (marker_pub.getNumSubscribers() < 1)
     {
@@ -61,25 +71,84 @@ int main( int argc, char** argv )
       ROS_WARN_ONCE("Please create a subscriber to the marker");
       sleep(1);
     }
-    marker_pub.publish(marker);
+    marker.lifetime = ros::Duration();
+    marker.header.stamp = ros::Time::now();
+    switch (state_var) {
+      // Publish the marker at the pickup zone
+      case 0:
+        ROS_INFO("Publishing marker to be picked up.");
+        // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+        marker.action = visualization_msgs::Marker::ADD;
+        // Set position and orientation of marker. 
+        marker.pose.position.x = x_goal;
+        marker.pose.position.y = y_goal;
+        marker.pose.position.z = 0;
+        marker.pose.orientation.x = 0.0;
+        marker.pose.orientation.y = 0.0;
+        marker.pose.orientation.z = 0.0;
+        marker.pose.orientation.w = w_goal;
+        //Publish the marker.
+        marker_pub.publish(marker);
+        ROS_INFO("Going to state 1...");
+        state_var = 1;
+        break;
+      case 1:
+        ROS_INFO("Waiting for robot to pick up object.");
+        sleep(1);
+        //See if radius is less than delta and then move to next state. 
+        x_length = fabs(marker.pose.position.x - x_Odom);
+        y_length = fabs(marker.pose.position.y - y_Odom);
+        rad = sqrt(pow(x_length, 2) + pow(x_length, 2));
+        if (rad < delta) { 
+             ROS_INFO("Radius is less than delta. Going to state 2...");
+            state_var = 2;
+        }
+        break;
 
-    // Cycle between different shapes
-    switch (shape)
-    {
-    case visualization_msgs::Marker::CUBE:
-      shape = visualization_msgs::Marker::SPHERE;
-      break;
-    case visualization_msgs::Marker::SPHERE:
-      shape = visualization_msgs::Marker::ARROW;
-      break;
-    case visualization_msgs::Marker::ARROW:
-      shape = visualization_msgs::Marker::CYLINDER;
-      break;
-    case visualization_msgs::Marker::CYLINDER:
-      shape = visualization_msgs::Marker::CUBE;
-      break;
+      case 2:
+        ROS_INFO("Marker has just been picked up. Now Hiding marker.");
+        sleep(2);
+        // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+        marker.action = visualization_msgs::Marker::DELETE;
+        // Publish marker
+        marker_pub.publish(marker);
+        ROS_INFO("Going to state 3...");
+        state_var = 3;
+        break;
+      case 3:
+        ROS_INFO("Waiting for marker to be dropped off at final location.");
+        sleep(2);
+        //See if radius is less than delta and then move to next state. 
+        x_length = fabs(marker.pose.position.x - x_Odom);
+        y_length = fabs(marker.pose.position.y - y_Odom);
+        rad = sqrt(pow(x_length, 2) + pow(x_length, 2));
+        if (rad < delta) { 
+             ROS_INFO("Radius is less than delta. Going to state 4...");
+            state_var = 4;
+        }
+        break;
+
+      case 4:
+        ROS_INFO("Final Destination Reached. Dropping of marker now.");
+        sleep(2);
+        // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+        marker.action = visualization_msgs::Marker::ADD;
+        // Set position and orientation of marker. 
+        marker.pose.position.x = x_goal2;
+        marker.pose.position.y = y_goal2;
+        marker.pose.position.z = 0;
+        marker.pose.orientation.x = 0.0;
+        marker.pose.orientation.y = 0.0;
+        marker.pose.orientation.z = 0.0;
+        marker.pose.orientation.w = w_goal2;
+        // Publish marker
+        marker_pub.publish(marker);
+        ROS_INFO("Congratulations! Marker has successfully reached it's final destination!");
+      default:
+        ROS_INFO("No valid state variable has been found.");
     }
-
+    ros::spinOnce();
     r.sleep();
   }
 }
+
